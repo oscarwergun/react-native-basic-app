50890e880d47e77c4f8f69037af14fb9d192020d
oscarwergun <127099022+oscarwergun@users.noreply.github.com>
2023-05-03 21:41:18 +0200

    initial setup

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("ozzy");
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>My name is {name}</Text>
       <StatusBar style="auto" />
+      <View style={styles.buttonContainer}>
+        <Button title="Click me" />
+      </View>
     </View>
   );
 }
@@ -13,8 +18,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  buttonContainer: {
+    padding:20,
+    
+  }
 });
